7a02508c3642c286d9d643f472b362082391008e
VaibhaVermA03 <vaibhavverma0307@gmail.com>
2026-02-09 18:43:45 +0530

    Fix React internal error by removing manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],  
   base: '/', 
 
   build: {
     chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
   }
 })
